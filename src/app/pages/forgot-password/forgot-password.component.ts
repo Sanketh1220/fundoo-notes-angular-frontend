a8bb9795172d22efc140bad5c4d5a715ac50fa06
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-07-07 17:36:48 +0530

    feat: ability to post forgot password from UI

--- src/app/pages/forgot-password/forgot-password.component.ts
+++ src/app/pages/forgot-password/forgot-password.component.ts
@@ -1,4 +1,6 @@
+import { UserService } from './../../services/user.service';
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  constructor() { }
+  constructor(private forgotPasswordService: UserService) { }
 
   ngOnInit(): void {
+  }
+
+  email = new FormControl('');
+
+  submit() {
+    var userData = {
+      email: this.email.value
+    }
+    console.log(userData);
+    this.forgotPasswordService.forgotPassword(userData).subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    })
   }
 
 }
